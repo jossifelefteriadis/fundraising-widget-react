1e969157dd1d85ba3e517d10447e91517deef7f8
Jossif Elefteriadis <Jossif_@hotmail.com>
2020-02-06 10:43:37 +0100

    Add widget and header components

--- fundraising-widget-react/src/App.js
+++ fundraising-widget-react/src/App.js
@@ -2,9 +2,12 @@ import React from 'react'
 import logo from './logo.svg'
 import './App.css'
 
+import Widget from './components/Widget'
+
 function App() {
   return (
     <div className='App'>
+      <Widget />
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
         <p>
